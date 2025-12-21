8f1f4125859193a41e6f54e95cd3e6be1d16e3b0
gabochaparro <143703358+gabochaparro@users.noreply.github.com>
2025-12-21 18:09:09 -0500

    Add library import handling to pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,1 +1,10 @@
-print("Prueba realizada con éxito")
+# Importar librerias
+#  -----------------------------------------------
+try:
+    import sys, logging, traceback, flask, pandas_ta as ta
+    from tvDatafeed import TvDatafeed, Interval
+except Exception as e:
+    print(f"ERROR IMPORTANDO LIBRERIAS: {e}")
+    sys.exit()
+#  -----------------------------------------------
+print("Librerias cargadas con éxito")
